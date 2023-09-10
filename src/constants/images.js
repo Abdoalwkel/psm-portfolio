5fc1430e3501af18f0dfa85369c28a766887d0a5
Abdoalwkel <134746268+Abdoalwkel@users.noreply.github.com>
2023-09-10 03:42:08 +0300

    Update images.js

--- src/constants/images.js
+++ src/constants/images.js
@@ -29,7 +29,7 @@ import bolt from '../assets/bolt.png';
 import nb from '../assets/nb.png';
 import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
-import bootstrap from '../assets/bootstrap.jpg';
+import bootstrap from '../assets/bootstrap.png';
 import github from '../assets/github.png'
 
 export default {
